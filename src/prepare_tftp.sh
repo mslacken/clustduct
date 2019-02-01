4553ec5b58df4124861556770dbe0695779a441a
Christian Goll <cgoll@suse.de>
2019-02-01 16:27:54 +0100

    fixed linebreak and made script more verbose

--- src/prepare_tftp.sh
+++ src/prepare_tftp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-TFTPDIR=${TFTPDIR+/srv/tftpdir}
-mkdir -p ${TFTPDIR}/clustduct
-mkdir -p ${TFTPDIR}/EFI/x86/
-cp /usr/share/syslinux/chain.c32 /usr/share/syslinux/menu.c32 /usr/share/syslinux/pxelinux.0 /usr/share/s
-yslinux/reboot.c32 $TFTPDIR
-cp /usr/lib64/efi/shim.efi ${TFTPDIR}/EFI/x86/bootx64.efi
-cp /usr/lib64/efi/grub.efi ${TFTPDIR}/EFI/x86/
+TFTPDIR=${TFTPDIR-/srv/tftpboot}
+cp -v /usr/share/syslinux/chain.c32 /usr/share/syslinux/menu.c32 /usr/share/syslinux/pxelinux.0 /usr/share/syslinux/reboot.c32 $TFTPDIR
+mkdir -pv ${TFTPDIR}/pxelinux.cfg/
+cp -v /usr/share/doc/clustduct/default.example ${TFTPDIR}/pxelinux.cfg/default
+mkdir -pv ${TFTPDIR}/EFI/x86/
+cp -v /usr/lib64/efi/shim.efi ${TFTPDIR}/EFI/x86/bootx64.efi
+cp -v /usr/lib64/efi/grub.efi ${TFTPDIR}/EFI/x86/
+cp -v /usr/share/doc/clustduct/grub.cfg ${TFTPDIR}/EFI/x86/
+mkdir -pv ${TFTPDIR}/clustduct
